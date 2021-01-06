89c0f3849efdccb588f0b24900e6bbda63137a00
Tanguy BERASATEGUY <tanguy.legeek@gmail.com>
2021-01-06 18:15:26 +0100

    parser levels.txt

--- src/main/java/fr/ubx/poo/game/Game.java
+++ src/main/java/fr/ubx/poo/game/Game.java
@@ -9,6 +9,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Optional;
 import java.util.Properties;
 
 import fr.ubx.poo.model.go.Bomb;
@@ -21,9 +23,11 @@ public class Game {
     private Monster[] monsters;
     private final String worldPath;
     public int initPlayerLives;
+    public int nbrLevels;
 
-    public Game(String worldPath) {
+    public Game(String worldPath) {	
         world = new WorldStatic();
+        WorldEntity[][] level1 = parse(worldPath+"/level1.txt");
         this.worldPath = worldPath;
         loadConfig(worldPath);
         Position positionPlayer = null;
@@ -54,9 +58,75 @@ public class Game {
             // load the configuration file
             prop.load(input);
             initPlayerLives = Integer.parseInt(prop.getProperty("lives", "3"));
+            nbrLevels = Integer.parseInt(prop.getProperty("levels","3"));
         } catch (IOException ex) {
             System.err.println("Error loading configuration");
         }
+    }
+    
+    private WorldEntity[][] parse(String worldPath) {
+    	try (InputStream input = new FileInputStream(worldPath)){
+    		byte[] byteArray = input.readAllBytes();
+    		input.close();
+    		int lineLength = 0;
+    		int nbrLines = 0;
+    		for(byte b:byteArray) {
+    			if((char)b!='\n') {
+    				lineLength++;
+    			}
+    			else {
+    				lineLength++;
+    				nbrLines++;
+    			}
+    		}
+    		lineLength/=nbrLines;
+    		WorldEntity world[][] = new WorldEntity[nbrLines][lineLength];
+    		for(int i=0; i<nbrLines; i++) {
+    			for(int j=0; j<lineLength; j++) {
+    				char c = (char)byteArray[i*lineLength+j];
+    				if(c == '\n') {
+    					break;
+    				}
+    				if(c == WorldEntity.BombNumberDec.getCode())
+    					world[i][j] = WorldEntity.BombNumberDec;
+    				if(c == WorldEntity.BombNumberInc.getCode())
+    					world[i][j] = WorldEntity.BombNumberInc;
+    				if(c == WorldEntity.BombRangeDec.getCode())
+    					world[i][j] = WorldEntity.BombRangeDec;
+    				if(c == WorldEntity.BombRangeInc.getCode())
+    					world[i][j] = WorldEntity.BombRangeInc;
+    				if(c == WorldEntity.Box.getCode())
+    					world[i][j] = WorldEntity.Box;
+    				if(c == WorldEntity.DoorNextClosed.getCode())
+    					world[i][j] = WorldEntity.DoorNextClosed;
+    				if(c == WorldEntity.DoorNextOpened.getCode())
+    					world[i][j] = WorldEntity.DoorNextOpened;
+    				if(c == WorldEntity.DoorPrevOpened.getCode())
+    					world[i][j] = WorldEntity.DoorPrevOpened;
+    				if(c == WorldEntity.Empty.getCode())
+    					world[i][j] = WorldEntity.Empty;
+    				if(c == WorldEntity.Heart.getCode())
+    					world[i][j] = WorldEntity.Heart;
+    				if(c == WorldEntity.Key.getCode())
+    					world[i][j] = WorldEntity.Key;
+    				if(c == WorldEntity.Monster.getCode())
+    					world[i][j] = WorldEntity.Monster;
+    				if(c == WorldEntity.Player.getCode())
+    					world[i][j] = WorldEntity.Player;
+    				if(c == WorldEntity.Princess.getCode())
+    					world[i][j] = WorldEntity.Princess;
+    				if(c == WorldEntity.Stone.getCode())
+    					world[i][j] = WorldEntity.Stone;
+    				if(c == WorldEntity.Tree.getCode())
+    					world[i][j] = WorldEntity.Tree;
+    			}
+    		}
+    		return world;
+    	}
+    	catch (IOException e){
+    		System.err.println("Error loading world");
+    	}
+		return null;
     }
 
     public World getWorld() {
